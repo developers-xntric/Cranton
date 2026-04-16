760d3b6e20ba19441b4b8528740ef748781a1b0d
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2026-04-16 12:22:48 +0500

    feat: initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { Geist } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 const rethinkSans = localFont({
   src: [
@@ -42,7 +46,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${rethinkSans.variable} ${onest.variable} h-full antialiased`}
+      className={cn("h-full", "antialiased", rethinkSans.variable, onest.variable, "font-sans", geist.variable)}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
