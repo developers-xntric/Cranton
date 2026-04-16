2f2881e56c8419208cd2f180480bea7f683c7eba
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2026-04-16 13:07:05 +0500

    Section 3

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -20,6 +20,10 @@ const About = () => {
                 title="Our Story"
                 image="/about/2.png"
                 isBlack={true}
+                description1={"Cranton Electrical Limited was founded with a clear objective to deliver reliable, high-performance helideck solutions for complex and demanding environments. From the very beginning, our focus has been on combining technical expertise with practical execution to meet the evolving needs of the aviation and offshore industries."}
+                description2={"Over the years, we have successfully delivered a wide range of helideck projects across multiple regions, working with both government and private sector clients. Our ability to consistently maintain high standards of quality, safety, and performance has helped us build long-term relationships based on trust and reliability."}
+                description3={"We specialize in the design, manufacturing, and installation of aluminium helidecks and associated systems, ensuring every solution is tailored to specific operational requirements. Our team works closely with clients, consultants, and external partners to ensure smooth execution at every stage of the project."}
+                description4={"We specialize in the design, manufacturing, and installation of aluminium helidecks and associated systems, ensuring every solution is tailored to specific operational requirements. Our team works closely with clients, consultants, and external partners to ensure smooth execution at every stage of the project."}
             />
         </div>
     )
--- components/about/value-card.tsx
+++ components/about/value-card.tsx
@@ -2,8 +2,10 @@ import Image from "next/image";
 
 interface ValueCardProps {
     title: string;
-    description1?: any;
-    description2?: any;
+    description1?: string;
+    description2?: string;
+    description3?: string;
+    description4?: string;
     image: string;
     isBlack?: boolean
 }
@@ -15,15 +17,21 @@ const defaultDescription2 = "Working with experts, vendors and other external pa
 
 const defaultDescription3 = "Our wide range of solutions focuses on areas where there are weight limitations and also provide low maintenance costs without compromising on the strength of structures. We cater to offering our services to suit every specific requirement to all brands and businesses locally and internationally.";
 
+const defaultDescription4 = "Our wide range of solutions focuses on areas where there are weight limitations and also provide low maintenance costs without compromising on the strength of structures. We cater to offering our services to suit every specific requirement to all brands and businesses locally and internationally.";
+
 export function ValueCard({
     title,
     image,
-    isBlack
+    isBlack,
+    description1,
+    description2,
+    description3,
+    description4,
 }: ValueCardProps) {
     return (
         <section className={`w-full ${isBlack ? "bg-black" : "bg-background"} py-12 md:py-20 md:px-8`}>
             <div className="2xl:max-w-350 w-[90%] mx-auto">
-                <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-6 direction-rtl items-center">
+                <div className={`grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-6 ${isBlack ? ' direction-rtl' : ''} items-center`}>
 
                     {/* Desktop Image */}
                     <div className="hidden md:flex justify-center">
@@ -42,22 +50,26 @@ export function ValueCard({
                     <div className="flex flex-col gap-2">
 
                         {/* Title */}
-                        <h2 className="text-[20px] md:text-3xl lg:text-[36px] font-medium leading-[1.2] text-[#0a0f1e] max-w-[50%]">
+                        <h2 className={`text-[20px] md:text-3xl lg:text-[36px] font-medium leading-[1.2]  ${isBlack ? 'text-end text-white' : 'text-start text-[#0a0f1e] max-w-1/2'}`}>
                             {title}
                         </h2>
 
-                        <p className="text-[14px] md:text-[14px] 2xl:text-[18px] leading-relaxed text-[#333] font-medium">
-                            {defaultDescription1}
+                        <p className={`text-[14px] md:text-[14px] 2xl:text-[18px] leading-relaxed text-[#333] font-medium ${isBlack ? 'text-end text-white opacity-80' : 'text-text-start'}`}>
+                            {description1 ? description1 : defaultDescription1}
                         </p>
 
 
-                        <p className="text-[14px] md:text-[14px] 2xl:text-[18px] leading-relaxed text-[#333] font-medium">
-                            {defaultDescription2}
+                        <p className={`text-[14px] md:text-[14px] 2xl:text-[18px] leading-relaxed text-[#333] font-medium ${isBlack ? 'text-end text-white opacity-80' : 'text-text-start'}`}>
+                            {description2 ? description2 : defaultDescription2}
                         </p>
 
-                        <p className="text-[14px] md:text-[14px] 2xl:text-[18px] leading-relaxed text-[#333] font-medium">
-                            {defaultDescription3}
+                        <p className={`text-[14px] md:text-[14px] 2xl:text-[18px] leading-relaxed text-[#333] font-medium ${isBlack ? 'text-end text-white opacity-80' : 'text-text-start'}`}>
+                            {description3 ? description3 : defaultDescription3}
                         </p>
+
+                        {description4 && <p className={`text-[14px] md:text-[14px] 2xl:text-[18px] leading-relaxed text-[#333] font-medium ${isBlack ? 'text-end text-white opacity-80' : 'text-text-start'}`}>
+                            {description4 ? description4 : defaultDescription4}
+                        </p>}
                     </div>
 
                     {/* Mobile Image */}
