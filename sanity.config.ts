ea79b4a41fa4dc370b0e0b898c805603be66b1c0
AbduSubhan11 <160301116+AbduSubhan11@users.noreply.github.com>
2026-09-03 15:53:32 +0500

    include Sanity studio config

---
+++ sanity.config.ts
@@ -0,0 +1,7 @@
+﻿import { defineConfig } from "sanity"
+import { structureTool } from "sanity/structure"
+import { visionTool } from "@sanity/vision"
+import { dataset, projectId } from "./sanity/env"
+import { schema } from "./sanity/schemaTypes"
+import { structure } from "./sanity/structure"
+export default defineConfig({ name: "cranton", title: "Cranton CMS", projectId: projectId || "missing-project-id", dataset, plugins: [structureTool({ structure }), visionTool()], schema })
